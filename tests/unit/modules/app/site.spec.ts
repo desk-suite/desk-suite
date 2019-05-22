92314722e3b61fb4c73a73a77701441183742528
diegorrod <30572826+diegorrod@users.noreply.github.com>
2019-05-22 16:28:56 -0300

    Update site.spec.ts

--- tests/unit/modules/app/site.spec.ts
+++ tests/unit/modules/app/site.spec.ts
@@ -1,6 +1,5 @@
 import { dataBase, finish } from "../../basePlugin";
-import { Site } from "../../../../src/modulos/app/site";
-import { database } from "firebase";
+import { Site, ISite } from "../../../../src/modulos/app/site";
 
 let resultAdd: string;
 const addOp = () =>
@@ -8,6 +7,14 @@ const addOp = () =>
     name: "Test-Site",
     logoURL: ""
   });
+
+let resultGet: ISite | undefined;
+const getOp = (value: string) => dataBase.collections.site.fetch(value);
+
+let resultUpdate: string;
+const updateOp = (value: ISite | undefined) =>
+  dataBase.collections.site.set(value as ISite);
+
 let resultDelete: string;
 const deleteOp = () => dataBase.collections.site.delete(resultAdd);
 
@@ -21,12 +28,37 @@ it("Verificar propiedades por defecto", () => {
 it("Add Site in Collection of Firestore", async () => {
   expect.assertions(1);
   resultAdd = await addOp();
+  console.log(resultAdd);
   expect(typeof resultAdd).toBe("string");
 });
+
+it("Get Site in Collection of Firestore", async () => {
+  resultGet = await getOp(resultAdd);
+  if (resultGet) {
+    console.log(resultGet.id);
+  }
+  expect(resultGet).not.toBeUndefined;
+});
+
+it("Update Site in Collection of Firestore", async () => {
+  expect.assertions(1);
+  resultUpdate = await updateOp(resultGet);
+  console.log(resultUpdate);
+  expect(typeof resultUpdate).toBe("string");
+});
+
 it("Delete Site in Collection of Firestore", async () => {
   expect.assertions(1);
   resultDelete = await deleteOp();
   expect(typeof resultDelete).toBe("string");
+});
+
+it("Final Result", () => {
+  expect(
+    resultAdd === resultGet!.id &&
+      resultAdd === resultUpdate &&
+      resultUpdate === resultDelete
+  ).toBe(true);
 });
 
 afterAll(async () => {
